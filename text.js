3b4cb2fad2adaa23e3c59adc381f65346ef931de
QudsiaAhmed <qudsiaahmed41@gmail.com>
2022-07-24 00:26:28 +0500

    New UI

--- text.js
+++ text.js
@@ -1,195 +1,63 @@
-setInterval(setClock, 1000);
+setInterval(() => {
 
-const hour_clock = document.querySelector('.hour');
-const min_clock = document.querySelector('.min');
-const sec_clock = document.querySelector('.sec');
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
 
-function setClock() {
-    const currentdate = new Date();
-    const sec = currentdate.getSeconds() / 60;
-    const min = (sec + currentdate.getMinutes()) / 60;
-    const hour = (min + currentdate.getHours()) / 12;
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
 
-    setRotation(hour_clock, hour);
-    setRotation(min_clock, min);
-    setRotation(sec_clock, sec);
-}
+    let hr_dot = document.querySelector('.hr_dot');
+    let min_dot = document.querySelector('.min_dot');
+    let sec_dot = document.querySelector('.sec_dot');
 
-function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotate', rotationRatio * 360);
-}
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let am = h >= 12 ? "PM" : "AM"
 
-
-
-
-
-
-
-
-
-
-
-
-
-function Clock() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-
-    var minute = date.getMinutes();
-
-    var seconds = date.getSeconds();
-
-    var day = date.getDay();
-
-    var today = date.getDate();
-
-    var month = date.getMonth();
-
-
-
-    if (day == 0) {
-
-        document.getElementById("day").innerHTML = "sunday"
-
-    } else if (day == 1) {
-
-        document.getElementById("day").innerHTML = "monday"
-
-    } else if (day == 2) {
-
-        document.getElementById("day").innerHTML = "tuesday"
-
-    } else if (day == 3) {
-
-        document.getElementById("day").innerHTML = "wednesday"
-
-    } else if (day == 4) {
-
-        document.getElementById("day").innerHTML = "thursday"
-
-    } else if (day == 5) {
-
-        document.getElementById("day").innerHTML = "friday"
-
-    } else if (day == 6) {
-
-        document.getElementById("day").innerHTML = "saturday"
-
+    // convert 24 hours clock to 12 hrs clock
+    if (h > 12) {
+        h = h - 12;
     }
+    //add zero before single digit number
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    hours.innerHTML = h + "<br><span>Hours</span>";
+    minutes.innerHTML = m + "<br><span>Minutes</span>";
+    seconds.innerHTML = s + "<br><span>Seconds</span>";
+    ampm.innerHTML = am
 
 
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+    // 12 hrs clocks
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    //60 minutes
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+    //60 seconds
+    hr_dot.style.transform = `rotate(${h * 30}deg)`;
+    //360/12=30
+    hr_min.style.transform = `rotate(${m * 6}deg)`;
+    //360/12=30
+    hr_sec.style.transform = `rotate(${s * 6}deg)`;
+    //360/12=30
+})
 
-    if (month == 0) {
+let hr = document.querySelector('#hr');
+let mn = document.querySelector('#mn');
+let sc = document.querySelector('#sc');
 
-        document.getElementById("date").innerHTML =
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
 
-            "january" + "  " + today;
-
-    } else if (month == 1) {
-
-        document.getElementById("date").innerHTML =
-
-            "february" + "  " + today;
-
-    } else if (month == 2) {
-
-        document.getElementById("date").innerHTML =
-
-            "march" + "  " + today;
-
-    } else if (month == 3) {
-
-        document.getElementById("date").innerHTML =
-
-            "april" + "  " + today;
-
-    } else if (month == 4) {
-
-        document.getElementById("date").innerHTML =
-
-            "may" + "  " + today;
-
-    } else if (month == 5) {
-
-        document.getElementById("date").innerHTML =
-
-            "june" + "  " + today;
-
-    } else if (month == 6) {
-
-        document.getElementById("date").innerHTML =
-
-            "july" + "  " + today;
-
-    } else if (month == 7) {
-
-        document.getElementById("date").innerHTML =
-
-            "august" + "  " + today;
-
-    } else if (month == 8) {
-
-        document.getElementById("date").innerHTML =
-
-            "september" + "  " + today;
-
-    } else if (month == 9) {
-
-        document.getElementById("date").innerHTML =
-
-            "october" + "  " + today;
-
-    } else if (month == 10) {
-
-        document.getElementById("date").innerHTML =
-
-            "november" + "  " + today;
-
-    } else if (month == 11) {
-
-        document.getElementById("date").innerHTML =
-
-            "december" + "  " + today;
-
-    }
-
-
-
-    if (hour > 12) {
-
-        hour = hour - 12;
-
-    }
-
-    if (hour < 10) {
-
-        hour = "0" + hour;
-
-    }
-
-    if (minute < 10) {
-
-        minute = "0" + minute;
-
-    }
-
-    if (seconds < 10) {
-
-        seconds = "0" + seconds;
-
-    }
-
-
-
-    document.getElementById("hour").innerHTML = hour;
-
-    document.getElementById("minute").innerHTML = minute;
-
-    document.getElementById("seconds").innerHTML = seconds;
-
-
-
-}
-setInterval(Clock, 1000)
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
